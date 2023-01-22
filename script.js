ae168fa556484a3da9d61e323b42610f9bca9995
ufbishal <ufbishal@gmail.com>
2023-01-22 10:24:10 -0600

    typing bug was fixed

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function loader() {
 
 //typing animation
 let i =0, writing;
-writing = " Welcome To My Portfolio \n I am Bishal"
+writing = " Welcome To My Portfolio \n  I am Bishal"
 console.log(writing);
 
 function typing() {
